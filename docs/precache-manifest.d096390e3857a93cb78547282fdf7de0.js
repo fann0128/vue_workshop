5d84557f2baa46af6cf8d1b5a6dd273dfe971ce4
fann0128 <fan.yang@osedea.com>
2019-12-04 12:16:26 -0500

    Added docs

---
+++ docs/precache-manifest.d096390e3857a93cb78547282fdf7de0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72315c3a7b26753d66a2",
+    "url": "/vue_workshop/css/app.578ae515.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/vue_workshop/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "22ee47cf2dabf285d98e9c727f749d4d",
+    "url": "/vue_workshop/index.html"
+  },
+  {
+    "revision": "72315c3a7b26753d66a2",
+    "url": "/vue_workshop/js/app.82b26caf.js"
+  },
+  {
+    "revision": "25bac733f2bcc9fdeb07",
+    "url": "/vue_workshop/js/chunk-vendors.ca7434c6.js"
+  },
+  {
+    "revision": "23844d3a5f1d3eea5cacddd74fe006cc",
+    "url": "/vue_workshop/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue_workshop/robots.txt"
+  }
+]);
